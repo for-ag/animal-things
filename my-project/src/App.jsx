403c66a8d121846ba012bb0d9cb77be838c05df1
Josh <145994834+for-ag@users.noreply.github.com>
2024-07-02 15:05:24 -0500

    updated jsx and animals

---
+++ my-project/src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+
+const dogURL = `https://dog.ceo/api/breeds/image/random`;
+const factURL = `https://catfact.ninja/fact`
+const catURL = `https://api.thecatapi.com/v1/images/search`
+const foxURL = `https://randomfox.ca/floof/`
+
+function App() {
+  
+  const [dog, setDog] = useState(null);
+  const [cat, setCat] = useState(null);
+  const [fact, setFact] = useState(null);
+  const [fox, setFox] = useState(null);
+
+  const fetchDog = async () => {
+    const result = await fetch(dogURL);
+    const json = await result.json();
+    setDog(json.message);
+  };
+
+  const fetchFact = async () => {
+    const result = await fetch(factURL);
+    const json = await result.json()
+    setFact(json.fact)
+  }
+
+  const fetchCat = async () => {
+    const result = await fetch(catURL);
+    const json = await result.json()
+    setCat(json[0].url)
+  }
+
+  const fetchFox = async () => {
+    const result = await fetch(foxURL);
+    const json = await result.json()
+    setFox(json.image)
+  }
+
+  useEffect(() => {
+    fetchDog();
+    fetchFact();
+    fetchCat();
+    fetchFox();
+  }, []);
+
+
+  return (
+    <>
+
+      <div className="content-center">
+
+        <header className="text-center">
+          <h1 className="text-2xl py-4 font-mono font-extrabold">Animal Things!</h1>
+        </header>
+
+        <div className="flex grid-col-3 object-contain mx-8 justify-around overflow-hidden bg-slate-600 rounded-md">
+          <img src={dog} alt="doges" className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300]"/>
+          <img src={cat} alt="big cat!" className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300]"/>
+          <img src={fox} alt="fox!"className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300]" />
+        </div>
+
+        <div className="text-center my-5 font-mono mx-8">
+          <p>{fact}</p>
+        </div>
+
+        <span></span>
+        
+        <div className="flex justify-between px-12 py-4 bg-slate-400 rounded-md mx-8">
+          <button onClick={fetchDog} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Dog!</button>
+          <button onClick={fetchFact} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Animal Fact!</button>
+          <button onClick={fetchCat} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Cat!</button>
+          <button onClick={fetchFox} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Fox!</button>
+        </div>
+      
+      </div>
+    
+    </>
+  );
+}
+
+export default App;

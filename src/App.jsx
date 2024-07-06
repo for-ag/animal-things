a9ceb7ef1d43b961e9aa7b6d09f6a55cf5bb244a
for-ag <145994834+for-ag@users.noreply.github.com>
2024-07-06 06:14:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -44,38 +44,38 @@ function App() {
   }, []);
 
 
- return (
-    <>
-  
-      <div className="content-center">
-  
-        <header className="text-center">
-          <h1 className="text-2xl py-4 font-mono font-extrabold">Animal Things!</h1>
-        </header>
-  
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-8 justify-center items-center overflow-hidden bg-slate-600 rounded-md">
-          <img src={dog} alt="doges" className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300px] mx-auto"/>
-          <img src={cat} alt="big cat!" className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300px] mx-auto"/>
-          <img src={fox} alt="fox!" className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300px] mx-auto"/>
-        </div>
-  
-        <div className="text-center my-5 font-mono mx-8">
-          <p>{fact}</p>
-        </div>
-  
-        <span></span>
-        
-        <div className="flex justify-between px-12 py-4 bg-slate-400 rounded-md mx-8">
-          <button onClick={fetchDog} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Dog!</button>
-          <button onClick={fetchFact} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Animal Fact!</button>
-          <button onClick={fetchCat} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Cat!</button>
-          <button onClick={fetchFox} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Fox!</button>
-        </div>
+return (
+  <>
+
+    <div className="content-center">
+
+      <header className="text-center">
+        <h1 className="text-2xl py-4 font-mono font-extrabold">Animal Things!</h1>
+      </header>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-8 justify-center items-center overflow-hidden bg-slate-600 rounded-md">
+        <img src={dog} alt="doges" className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300px] mx-auto"/>
+        <img src={cat} alt="big cat!" className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300px] mx-auto"/>
+        <img src={fox} alt="fox!" className="object-scale-down max-w-[300px] max-h-[300px] min-h-[300px] mx-auto"/>
+      </div>
+
+      <div className="text-center my-5 font-mono mx-8">
+        <p>{fact}</p>
+      </div>
+
+      <span></span>
       
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-12 py-4 bg-slate-400 rounded-md mx-8">
+        <button onClick={fetchDog} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Dog!</button>
+        <button onClick={fetchFact} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Animal Fact!</button>
+        <button onClick={fetchCat} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Cat!</button>
+        <button onClick={fetchFox} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Random Fox!</button>
       </div>
     
-    </>
-  );
-  }
+    </div>
   
-  export default App;
+  </>
+);
+}
+
+export default App;
